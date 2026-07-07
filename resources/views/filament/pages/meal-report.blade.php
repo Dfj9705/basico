416b495294633fb94576698f4f5c49ad223cf293
Abner Daniel Fuentes Juárez <abnerfuentes05@gmail.com>
2026-07-07 09:44:52 -0600

    fix: quita head de dias

--- resources/views/filament/pages/meal-report.blade.php
+++ resources/views/filament/pages/meal-report.blade.php
@@ -11,19 +11,6 @@
 
     <div class="mt-6 overflow-x-auto rounded-xl border border-gray-200 dark:border-gray-700">
         <table class="w-full text-sm">
-            <thead>
-                <tr class="bg-gray-100 dark:bg-gray-800">
-                    <th class="px-4 py-2 text-left">Catálogo</th>
-                    <th class="px-4 py-2 text-left">Grado</th>
-                    <th class="px-4 py-2 text-left">Arma</th>
-                    <th class="px-4 py-2 text-left">Nombre</th>
-                    <th class="px-4 py-2 text-center">Lunes</th>
-                    <th class="px-4 py-2 text-center">Martes</th>
-                    <th class="px-4 py-2 text-center">Miércoles</th>
-                    <th class="px-4 py-2 text-center">Jueves</th>
-                    <th class="px-4 py-2 text-center">Viernes</th>
-                </tr>
-            </thead>
             <tbody>
                 @forelse ($this->getRecords() as $record)
                     <tr class="border-t border-gray-200 dark:border-gray-700">
